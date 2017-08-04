8cd51b6e616ad41de3d0867ef1bb8751e2fa3317
Thura Tun <thura.kofat@gmail.com>
2017-08-04 15:48:32 +0630

    FirstCommit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Get = new System.Windows.Forms.Button();
+            this.btn_SaveData = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_ShowImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Get
+            // 
+            this.btn_Get.Location = new System.Drawing.Point(35, 13);
+            this.btn_Get.Name = "btn_Get";
+            this.btn_Get.Size = new System.Drawing.Size(75, 23);
+            this.btn_Get.TabIndex = 0;
+            this.btn_Get.Text = "GetData";
+            this.btn_Get.UseVisualStyleBackColor = true;
+            this.btn_Get.Click += new System.EventHandler(this.btn_Get_Click);
+            // 
+            // btn_SaveData
+            // 
+            this.btn_SaveData.Location = new System.Drawing.Point(140, 12);
+            this.btn_SaveData.Name = "btn_SaveData";
+            this.btn_SaveData.Size = new System.Drawing.Size(75, 23);
+            this.btn_SaveData.TabIndex = 1;
+            this.btn_SaveData.Text = "SetData";
+            this.btn_SaveData.UseVisualStyleBackColor = true;
+            this.btn_SaveData.Click += new System.EventHandler(this.btn_SaveData_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(516, 282);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(307, 15);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(151, 20);
+            this.txt_Name.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_ShowImage
+            // 
+            this.btn_ShowImage.Location = new System.Drawing.Point(307, 68);
+            this.btn_ShowImage.Name = "btn_ShowImage";
+            this.btn_ShowImage.Size = new System.Drawing.Size(101, 23);
+            this.btn_ShowImage.TabIndex = 4;
+            this.btn_ShowImage.Text = "ChooseImage";
+            this.btn_ShowImage.UseVisualStyleBackColor = true;
+            this.btn_ShowImage.Click += new System.EventHandler(this.btn_ShowImage_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(557, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(781, 702);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_ShowImage);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_SaveData);
+            this.Controls.Add(this.btn_Get);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Get;
+        private System.Windows.Forms.Button btn_SaveData;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_ShowImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+
+        public string FilePath { get; set; }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void SaveData()
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("mydb");
+            var collection = db.GetCollection<Stock>("stock");
+
+            Stock _stknew = new Stock { name = txt_Name.Text , image = ConvertImageFileToByte(this.FilePath) };
+            collection.InsertOne(_stknew);
+            this.FilePath = null;
+            GetData();
+
+        }
+
+        public  void GetData()
+        {
+
+            var credential = MongoCredential.CreateCredential("mydb", "sa", "015427");
+
+            var setting = new MongoClientSettings
+            {
+                Credentials = new[] { credential },
+                Server = new MongoServerAddress("localhost", 27017)
+
+            };
+
+            //MongoClient client = new MongoClient("mongodb://localhost:27017");
+            MongoClient client = new MongoClient(setting);
+            
+            var db = client.GetDatabase("mydb");
+            
+              
+            var collection = db.GetCollection<Stock>("stock");
+       
+
+            var result =  collection.AsQueryable<Stock>().ToList();
+
+
+            this.dataGridView1.DataSource = result;
+            this.dataGridView1.Columns[0].Visible = false;
+
+        }
+
+        private void btn_Get_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private void btn_SaveData_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void btn_ShowImage_Click(object sender, EventArgs e)
+        {
+           DialogResult result = openFileDialog1.ShowDialog();
+            this.FilePath = openFileDialog1.FileName;
+        }
+
+
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            
+        }
+        public Byte[] ConvertImageFileToByte(string Path)
+        {
+            if (String.IsNullOrEmpty(Path))
+            {
+                return null;
+            }
+            else
+            {
+                return File.ReadAllBytes(Path);
+            }
+
+        }
+        public Image byteArrayToImage(byte[] bytesArr)
+        {
+            MemoryStream memstr = new MemoryStream(bytesArr);
+            Image img = Image.FromStream(memstr);
+            return img;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (this.dataGridView1.Rows[e.RowIndex].Cells["image"].Value is Byte[])
+            {
+                this.pictureBox1.Image = byteArrayToImage((Byte[])this.dataGridView1.Rows[e.RowIndex].Cells["image"].Value);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                this.pictureBox1.Image = null;
+            }
+        }
+    }
+
+    public class Stock
+    {
+        public ObjectId Id { get; set; }
+        public string name { get; set; }
+
+        public byte[] image { get; set; }
+    }
+}
+
